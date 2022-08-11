fb7c69b89575be2f82349841e4ded09db01d76a2
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-08-11 19:06:23 +0700

    Add Hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,58 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material"
+import {
+  AppBar,
+  Button,
+  Container,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material"
 import { Slideshow } from "@mui/icons-material"
+import { Swiper, SwiperSlide } from "swiper/react"
 import Search from "./Search/Search"
+import SaintSeiya from "../assets/images/saint-seiya-banner 1.png"
+
+export const StyledImage = styled("img")(({ theme }) => ({
+  height: "24rem",
+  width: "100%",
+  objectFit: "cover",
+}))
 
 const Header = () => {
   return (
-    <AppBar sx={{ backgroundColor: "primary.contrastText" }} position="static">
-      <Container>
-        <Toolbar sx={{ justifyContent: "space-between" }} disableGutters>
-          <Toolbar disableGutters>
-            <Slideshow fontSize="large" color="primary" />
-            <Typography
-              sx={{ display: { xs: "none", sm: "block" } }}
-              component="h1"
-              color="secondary.contrastText"
-            >
-              Bivie
-            </Typography>
-            <Search />
+    <>
+      <AppBar
+        sx={{ backgroundColor: "primary.contrastText" }}
+        position="static"
+      >
+        <Container>
+          <Toolbar sx={{ justifyContent: "space-between" }} disableGutters>
+            <Toolbar disableGutters>
+              <Slideshow fontSize="large" color="primary" />
+              <Typography
+                sx={{ display: { xs: "none", sm: "block" } }}
+                component="h1"
+                color="secondary.contrastText"
+              >
+                Bivie
+              </Typography>
+              <Search />
+            </Toolbar>
+            <Button>Login</Button>
           </Toolbar>
-          <Typography component="h2" color="secondary.contrastText">
-            Sign in
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        </Container>
+      </AppBar>
+      <Swiper loop={true}>
+        <SwiperSlide>
+          <StyledImage src={SaintSeiya} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <StyledImage src={SaintSeiya} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <StyledImage src={SaintSeiya} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
   )
 }
 
