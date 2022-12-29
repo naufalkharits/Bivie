e230240d6b9fdef328e66e74fa70825434eeee9a
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-29 18:57:28 +0700

    refactor: promise to async-await

--- src/hooks/useGetMovies.jsx
+++ src/hooks/useGetMovies.jsx
@@ -1,18 +1,22 @@
 import { ReactLocation } from "@tanstack/react-location"
 import { useQuery } from "@tanstack/react-query"
 
-export const getMovies = (page) =>
-  fetch(
-    `${
-      import.meta.env.VITE_TMDB_API_BASE_URL
-    }/discover/movie?page=${page}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
-    {
-      method: "GET",
-    }
-  )
-    .then((response) => response.json())
-    .catch((error) => console.error(error))
+export const getMovies = async (page) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_TMDB_API_BASE_URL}/discover/movie?page=${page}&api_key=${
+        import.meta.env.VITE_TMDB_API_KEY
+      }`,
+      {
+        method: "GET",
+      }
+    )
 
+    return await response.json()
+  } catch (error) {
+    return console.error(error)
+  }
+}
 const useGetMovies = () => {
   const location = new ReactLocation()
   return useQuery(["movies"], () =>
