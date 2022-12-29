cb28166643a7e6fe7928f6e3096bf32d65efecdf
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-29 22:49:41 +0700

    fix: add loopedSlides to fix active slide

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { StyledImage } from "./Header.styled"
 
 const Header = (props) => {
   return (
-    <Swiper modules={[Pagination]} loop={true} pagination={{ clickable: true }}>
+    <Swiper modules={[Pagination]} loop={true} loopedSlides="3" pagination={{ clickable: true }}>
       {props.trendingMovies.data?.results.slice(0, 3).map((movie) => (
         <SwiperSlide key={movie.id}>
           <StyledImage
