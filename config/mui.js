0d63c360ddf94e39146f8f73d2bd6f1e3ad27d62
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-29 20:31:18 +0700

    refactor: change contrastText on secondary palette

--- config/mui.js
+++ config/mui.js
@@ -3,16 +3,12 @@ import { createTheme } from "@mui/material/styles"
 const mui = createTheme({
   palette: {
     primary: {
-      // light: "#",
       main: "#FE024E",
-      // dark: "#",
       contrastText: "#fff",
     },
     secondary: {
-      // light: "#",
       main: "#EB507F",
-      // dark: "#",
-      contrastText: "#000",
+      contrastText: "#fff",
     },
   },
 })
