7528d99e339ce8db8bfd1713dd0913090ccd5cd5
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-30 21:53:20 +0700

    refactor: change page-button align

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,23 +1,25 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack"
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
-import { Pagination, PaginationItem } from "@mui/material"
+import { Pagination, PaginationItem, Stack } from "@mui/material"
 
 const PageButton = (props) => {
   return (
-    <Pagination
-      page={props.router.state.currentLocation.search.page || 1}
-      count={500}
-      shape="rounded"
-      color="secondary"
-      size="large"
-      renderItem={(item) => (
-        <PaginationItem
-          slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-          href={item.page === 1 ? "/" : `?page=${item.page}`}
-          {...item}
-        />
-      )}
-    />
+    <Stack alignItems="center">
+      <Pagination
+        page={props.router.state.currentLocation.search.page || 1}
+        count={500}
+        shape="rounded"
+        color="secondary"
+        size="large"
+        renderItem={(item) => (
+          <PaginationItem
+            slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+            href={item.page === 1 ? "/" : `?page=${item.page}`}
+            {...item}
+          />
+        )}
+      />
+    </Stack>
   )
 }
 
