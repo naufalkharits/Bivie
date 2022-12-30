ff3d59b1ad7f252f2c0098b33e7462dc5fc2d86b
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-30 23:06:12 +0700

    refactor: add animation to movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,18 @@
 import { Card, CardContent, CardMedia, Typography, Unstable_Grid2 as Grid } from "@mui/material"
+import { motion } from "framer-motion"
 
 const MovieCard = (props) => {
   return (
-    <Grid xs={6} sm={4} md={3} lg={2}>
+    <Grid
+      component={motion.div}
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      xs={6}
+      sm={4}
+      md={3}
+      lg={2}>
       <Card sx={{ borderRadius: "0.5rem" }}>
         <CardMedia
           component="img"
