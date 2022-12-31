3d6a060ace1d59762e115f87273a5a78c4d84ed5
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-31 21:40:46 +0700

    refactor: prettier

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -24,7 +24,7 @@ const MovieCard = (props) => {
       <Card sx={{ borderRadius: "0.5rem" }}>
         <CardMedia
           component="img"
-          height="264"
+          height={264}
           loading="lazy"
           image={`${import.meta.env.VITE_TMDB_API_IMAGE_URL}/w500/${props.movie.poster_path}`}
           alt=""
